050d289162f388ba60986fee32c338b6b2639e16
mkverma541 <mkverma541@gmail.com>
2022-05-25 14:42:48 +0530

    responsive design fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ document.addEventListener('click', function(e) {
         e.target.children[0].classList.toggle('active');
     }
 })
+
+
+const header = document.querySelector('header')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > header.offsetHeight + 150) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
